7ce9a57e36d730c35e768e9d63bc0612056e2c7a
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-06 10:07:36 +0100

    Implementing the UI for the app. Relating buttons with function.

--- EggTimerUdemy/ViewController.swift
+++ EggTimerUdemy/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        print(sender.currentTitle)
+    }
 
 }
 
