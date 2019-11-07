f6fcccfc26530121560533894052e1cd821d7bca
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-07 12:39:27 +0100

    Setting the functionality  for every button. Setting timer. Implementing the sound when the progressView is finished.

--- EggTimerUdemy/ViewController.swift
+++ EggTimerUdemy/ViewController.swift
@@ -7,17 +7,58 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
+    var timer = Timer()
+    var audioPlayer: AVAudioPlayer!
+    
+    let eggTime =  [
+                    "SOFT EGG" : 300,
+                    "MEDIUM EGG" : 420,
+                    "HARD EGG" : 720
+                    ]
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        print(sender.currentTitle)
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        totalTime = eggTime[hardness]!
+        
+        progressView.progress = 0.0
+        secondsPassed = 0
+        timerLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
-
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            timerLabel.text = "DONE!"
+            theEggIsDone(soundName: "alarm_sound")
+        }
+    }
+    
+    func theEggIsDone(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+               audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+               audioPlayer?.play()
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertVC, animated: true, completion: nil)
+    }
 }
 
